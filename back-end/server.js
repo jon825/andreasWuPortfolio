cb0a159caa65734833a41e3e8348ea6b94acbab9
Jonathan Kok <kui825@gmail.com>
2018-02-24 19:49:11 -0500

    server

--- back-end/server.js
+++ back-end/server.js
@@ -16,12 +16,12 @@ const accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 
 app.use(bodyParser); // for parsing application/json
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   next();
+// });
 
 //i need fileUpload() to send my response from my front end to my backend
 //but this does not same my image file to my uplaods
